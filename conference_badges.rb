fae895d6dc98fab240556bf000d4ef713c9c740c
jennafritz <jennafritz24@gmail.com>
2021-05-16 19:00:15 -0400

    commit tree 82c223f9514440b04ac4c8727b287f46a83f35d1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+    messages_array = []
+    names_array.each do |name|
+        messages_array << badge_maker(name)
+    end
+    messages_array
+end
+
+# def assign_rooms(names_array)
